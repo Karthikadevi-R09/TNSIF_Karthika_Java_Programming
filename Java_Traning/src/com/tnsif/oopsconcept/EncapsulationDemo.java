8c93aa207dbb177d939755c17a4b475a8e8b348f
karthikadevi-R09 <sumathikarathika@gmail.com>
2025-10-28 21:11:42 +0530

    oopsconcept

---
+++ Java_Traning/src/com/tnsif/oopsconcept/EncapsulationDemo.java
@@ -0,0 +1,18 @@
+package com.tnsif.oopsconcept;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+
+		Encapsulation ed = new Encapsulation();
+		ed.setName("MS Dhoni");
+		ed.setAge(43);
+		ed.setJersyNo(7);
+		System.out.println(ed);
+
+		
+		EncapsulationDemo e = new EncapsulationDemo(); System.out.println(e);
+		 
+
+	}
+
+}
